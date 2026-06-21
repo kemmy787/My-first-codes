18aa57891d5f136c827f788501ce3fd24ac6297c
kemmy787 <iamgilliankemunto@gmail.com>
2026-06-22 00:06:06 +0300

    comparisonOperator

---
+++ Java/comparisonOperator.js
@@ -0,0 +1,17 @@
+let numb1=23
+let numb2=23
+let Strictequal=numb1===numb2
+console.log(`numb1=${numb1}===numb2=${numb2} 
+    Ans ${Strictequal} and type is ${typeof Strictequal}`)
+ let nonStrictEqual=numb1==numb2
+ console.log(`numb1=${numb1}==numb2=${numb2}
+    Ans ${nonStrictEqual} and type is ${typeof nonStrictEqual}`)
+
+    let val1=23
+    let val2='23'
+    let strictinequal=val1!==val2
+    console.log(`val1=${val1}!==val2=${val2}
+    Ans ${strictinequal} and type is ${typeof strictinequal}`)
+    let nonStrictInequal=val1!=val2
+    console.log(`val1=${val1}!=val2=${val2}
+    Ans ${nonStrictInequal} and type is ${typeof nonStrictInequal}`)
